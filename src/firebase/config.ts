887067e785e3bf6312508050796ca5a3e877f1b2
weckman <weckman@gmail.com>
2025-11-28 21:48:54 +0000

    Эта ошибка говорит о том, что при инициализации Firebase в вашем коде не

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+// src/firebase/config.ts
 export const firebaseConfig = {
-  "projectId": "studio-7924146673-6e291",
-  "appId": "1:569166457646:web:e559fd30424d06e52128bf",
-  "apiKey": "AIzaSyBPgEnznuxTEXvq4Jd27yeCRT4qEfsqp8w",
-  "authDomain": "studio-7924146673-6e291.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "569166457646"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
