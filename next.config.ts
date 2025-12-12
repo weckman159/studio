c8b3b94a6cf406098b3986020986ea53043f9d56
weckman <weckman@gmail.com>
2025-12-12 20:08:39 +0000

    Fix the next/image error by adding i.pravatar.cc to remotePatterns in ne

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         hostname: '*.public.blob.vercel-storage.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
