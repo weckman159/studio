fade65eba6b410eee786f52956985727a71a8b8a
weckman <weckman@gmail.com>
2025-12-21 19:29:44 +0000

    20:24:34.758 Running build in Washington, D.C., USA (East) – iad1 20:24:

---
+++ src/app/(admin)/admin/page.tsx
@@ -0,0 +1,5 @@
+// This page is deactivated to resolve a routing conflict.
+// The active page is now located in /app/admin/page.tsx
+export default function DeactivatedAdminPage() {
+  return null;
+}
