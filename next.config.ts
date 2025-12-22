a879022e86d34fc393106ff599bd5ad171689190
weckman <weckman@gmail.com>
2025-12-22 12:56:59 +0000

    делай

--- next.config.ts
+++ next.config.ts
@@ -9,12 +9,6 @@ const withPWA = require('next-pwa')({
 });
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   experimental: {
     optimizePackageImports: ['@radix-ui/react-icons', 'lucide-react'],
   },
